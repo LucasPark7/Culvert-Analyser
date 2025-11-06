d34192fa8828415d1b65a5f2f263d948f5026820
Lucas <lucas.park7@gmail.com>
2025-11-06 03:11:01 -0800

    chart color change

--- assets/js/analyse-page.js
+++ assets/js/analyse-page.js
@@ -30,7 +30,7 @@ chartInstance = new Chart(ctx, {
         datasets: [{
         label: "Culvert Score",
         backgroundColor:"rgba(0,0,255,1.0)",
-        borderColor: "rgba(0, 0, 255, 0.3)",
+        borderColor: "rgba(20, 179, 228, 1)",
         data: []
         }]
     },
