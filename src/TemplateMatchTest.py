5ff53c287ffe513588bf4cc5b9d31b2c281dcb4e
Lucas <lucas.park7@gmail.com>
2025-11-07 03:04:45 -0800

    edge based template match test

--- src/TemplateMatchTest.py
+++ src/TemplateMatchTest.py
@@ -50,9 +50,8 @@ while True:
     gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     grayFatal = cv2.cvtColor(fatal, cv2.COLOR_BGR2GRAY)
 
-    orb = cv2.ORB_create()
-    kp1, des1 = orb.detectAndCompute(gray,None)
-    kp2, des2 = orb.detectAndCompute(grayFatal,None)
+    img = cv2.equalizeHist(gray)
+    template = cv2.equalizeHist(grayFatal)
 
     if roi.size > 0:
         cv2.imshow("Cropped ROI", gray)
@@ -64,26 +63,13 @@ while True:
     if cv2.waitKey(30) & 0xFF == ord("q"):
         
         # template matching
-        res = cv2.matchTemplate(gray, grayFatal, cv2.TM_CCOEFF_NORMED)
+        #res = cv2.matchTemplate(gray, grayFatal, cv2.TM_CCOEFF_NORMED)
+        #min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+        edges_img = cv2.Canny(img, 50, 150)
+        edges_template = cv2.Canny(template, 50, 150)
+        res = cv2.matchTemplate(edges_img, edges_template, cv2.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
-
-        # create BFMatcher object
-        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-        if des1 is None or des2 is None:
-            print("No descriptors found!")
-        print(grayFatal.shape, gray.shape)
-        
-        # Match descriptors.
-        matches = bf.match(des1,des2)
-        
-        # Sort them in the order of their distance.
-        matches = sorted(matches, key = lambda x:x.distance)
-        
-        # Draw first 10 matches.
-        img3 = cv2.drawMatches(gray,kp1,grayFatal,kp2,matches[:10],None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        
-        plt.imshow(img3),plt.show()
 
         threshold = 0.75
         print(max_val)
