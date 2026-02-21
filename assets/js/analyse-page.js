289e00bb3c55272edbd7054f0c4c45b015a8e205
Lucas <lucas.park7@gmail.com>
2026-02-21 01:32:51 -0800

    logging for easyocr results

--- assets/js/analyse-page.js
+++ assets/js/analyse-page.js
@@ -210,8 +210,9 @@ async function uploadVideo() {
             dataSet.forEach(function (value, index) {
                 // if frame isn't in dataset yet then we can push, otherwise ignore
                 if ((index + 1) > new_culvert.frames[new_culvert.frames.length - 1]) {
+                    console.log(value);
                     new_culvert.frames.push(index + 1);
-                    new_culvert.values.push(value[0]);
+                    new_culvert.values.push(int(value[0][0]));
                     new_culvert.fatal_list.push(value[1]);
                 }
             });
