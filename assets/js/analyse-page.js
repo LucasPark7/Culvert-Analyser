909e5ab5f413e4a2823ce82530208b5a37538e2d
Lucas <lucas.park7@gmail.com>
2026-02-27 04:57:30 -0800

    repopulate fix

--- assets/js/analyse-page.js
+++ assets/js/analyse-page.js
@@ -122,7 +122,6 @@ function computeStats(culvert_data) {
 function addRun(new_culvert, index) {
     let new_list_run = culvList.insertRow(-1);
     let runCell = new_list_run.insertCell(0);
-    console.log(new_culvert.values);
     runCell.textContent = "Culvert Run #" + culvList.rows.length + " (" + new_culvert.values[new_culvert.values.length - 1] + ")";
     runCell.style.cursor = 'pointer';
     runCell.dataset.culvert_data = JSON.stringify(new_culvert);
@@ -153,14 +152,15 @@ function addRun(new_culvert, index) {
 // repopulate run list with saved scores
 function populateList() {
     for (let i = 0; i < list_runs.length - 1; i++) {
-        addRun(list_runs[i], i);
+        const culv_data = JSON.parse(list_runs[i]);
+        addRun(culv_data, i);
 
         // segmenting for fatals
-        const fatal = (ctx, value) => list_runs[i].fatal_list[ctx.p0DataIndex] ? value : undefined;
+        const fatal = (ctx, value) => culv_data.fatal_list[ctx.p0DataIndex] ? value : undefined;
 
         chartInstance.data.datasets.push({
             label: "Culvert #" + (i + 1),
-            data: list_runs[i].values,
+            data: culv_data.values,
             borderColor: "rgb(255, 255, 255)",
             backgroundColor: "rgb(255, 255, 255)",
             segment: { borderColor: ctx => fatal(ctx, 'rgb(192,75,75)') },
