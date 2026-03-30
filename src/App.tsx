4acbea3c13ebbe79ef636120dabd52c6192c6392
Lucas <lucas.park7@gmail.com>
2026-03-30 03:53:26 -0700

    typo fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Analyse from './pages/Analyse';
-// import Home from './pages/Home';       // add other pages here as you migrate them
-// import OtherPage from './pages/OtherPage';
 
 export default function App() {
   return (
